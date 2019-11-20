5272f60d8548ac527b68a4021ec8a5c722deb2f3
Richar Oviedo <richareduaro28@gmail.com>
2019-11-20 10:00:22 -0300

    Version 2.9

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -44,7 +44,7 @@
         </nav>
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <a href="{{url('home')}}" class="brand-link">
-                <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+                <img src="wolfsyncro/adminlte3/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                     style="opacity: .8">
                 <span class="brand-text font-weight-light">AdminLTE 3</span>
             </a>
