cb3a3f8f8db8e4851e3614742d4d5a3d457bb80f
richaroviedo28 <richareduaro28@gmail.com>
2020-05-15 17:12:02 -0400

    Modificacion de Vista de Recuperacion de password

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-adminlte3
+Instalacion del Paquete Adminlte3
+
+1º Paso
+Instalar el paquete.
+
+- composer require wolfsyncro/adminlte3
+
+2º Paso
+Publicar los archivos.
+
+- php artisan vendor:publish --provider="wolfsyncro\adminlte3\Adminlte3ServiceProvider" --force
+
+Usamos --force para sobreescribir algunos archivos.
+Archivos que se sobreescriben
+- welcome.blade.php
+- home.blade.php
+- login.blade.php
+- register.blade.php
+- reset.blade.php
---
+++ views/auth/passwords/email.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.auth')
+
+@section('content')
+<div class="login-box">
+  <div class="login-logo">
+  <a href="{{url('/')}}"><b>Admin</b>LTE Version 3</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">{{ __('Reset Password') }}</p>
+      @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
+    <form method="POST" action="{{ route('password.email') }}">
+        @csrf
+        <div class="input-group mb-3">
+          <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+          @error('email')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary btn-block">{{ __('Send Password Reset Link') }}</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <p class="mt-3 mb-1">
+        <a href="{{route('login')}}">Ya Tengo una Cuenta</a>
+      </p>
+      <p class="mb-0">
+        <a href="{{route('register')}}" class="text-center">Registrar Nuevo Usuario</a>
+      </p>
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+@endsection
